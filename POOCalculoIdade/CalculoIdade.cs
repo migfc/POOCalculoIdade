c7bdfa0f579f9f4ca29b1b4c45cea2fa30020b95
migfc <miguel.freitas.coimbra18@gmail.com>
2024-10-22 17:30:05 -0300

    Adicionar arquivos de projeto.

---
+++ POOCalculoIdade/CalculoIdade.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOCalculoIdade
+{
+    internal class CalculoIdade
+    {
+        private int anoNacimento;
+
+        public int AnoNacimento
+        {
+            get { return this.anoNacimento; }
+            set { this.anoNacimento = value; }
+        }
+        private string nome = "";
+        
+        public string Nome
+        {
+            get { return this.nome; }
+            set { this.nome = value; }
+        }
+        private int CalcularIdade()
+        {
+            int anoHoje, resultado;
+            DateTime data = DateTime.Now;
+            anoHoje = data.Year;
+            resultado = anoHoje - this.anoNacimento;
+            return resultado;
+        }
+        public void ExibirDados()
+        {
+            Console.WriteLine("seu nome é: {0}", this.Nome);
+            Console.WriteLine("ano de nacimento: {0}",this.AnoNacimento);
+            Console.WriteLine("idade: {0}", CalcularIdade());
+        }
+    }
+}
---
+++ POOCalculoIdade/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using POOCalculoIdade;
+
+CalculoIdade POO0 = new CalculoIdade();
+Console.WriteLine("progrma que calcula a idade de uma pesoa");
+Console.Write("digite o nome: ");
+POO0.Nome = Console.ReadLine() ?? "";
+Console.Write("digite o ano de nacimento: ");
+POO0.AnoNacimento = Convert.ToInt32(Console.ReadLine() ?? "");
+POO0.ExibirDados();
